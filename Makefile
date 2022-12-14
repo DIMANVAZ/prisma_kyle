29dddff12aaa6358333fb8287f0f9b3643df4d3d
DIMANVAZ <DIMANVAZ04@GMAIL.COM>
2022-12-14 20:29:52 +0300

    finalle

--- Makefile
+++ Makefile
@@ -1,2 +1,10 @@
 align:
 	npx prisma format
+gen:
+	npx prisma generate
+mig:
+	npx prisma migrate dev --name autoNamedMig
+stu:
+	npx prisma studio
+go:
+	npm run devStart
--- main.ts
+++ main.ts
@@ -1,18 +1,61 @@
 import {PrismaClient} from '@prisma/client';
+
 const prisma = new PrismaClient();
 
-async function main(){
+async function creating(){
     const creationResult = await prisma.user.create({
         data: {
-            name: 'Ayrat',
+            name: 'Hanaf',
             isAdmin: true,
-            email: "ay@mail.ru"
-        }
+            email: "hanaf@eu.com",
+            userPreference: {
+                create:{
+                    emailUpdates: true
+                },
+            },
+        },
+        select:{
+            name: true,
+            userPreference: true
+        },
     });
-    console.log(creationResult);
+    console.log('created:',creationResult);
 }
 
-main()
+async function reading(){
+    const foundFirst = await prisma.user.findFirst({where: {
+        isAdmin: true
+    }});
+    console.log('foundFirst:',foundFirst);
+}
+
+async function updating(){
+    const updated = await prisma.user.updateMany({
+        where: {
+            isAdmin: false,
+        },
+        data: {
+            name: "Кто-то неважный"
+        },
+    });
+    console.log('updated:',updated);
+}
+
+// creating()
+//     .catch(e => console.log(e))
+//     .finally(async() => {
+//         await prisma.$disconnect();
+//         console.log('disconnected');
+//     })
+
+// reading()
+//     .catch(e => console.log(e))
+//     .finally(async() => {
+//         await prisma.$disconnect();
+//         console.log('disconnected');
+//     })
+
+updating()
     .catch(e => console.log(e))
     .finally(async() => {
         await prisma.$disconnect();
